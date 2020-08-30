9828a09d4f40e2a791a99f697fb9fb1a91f65312
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-08-30 16:11:36 -0400

    fixed save btn functionality again

--- scripts.js
+++ scripts.js
@@ -14,27 +14,41 @@ window.addEventListener('load', function () {
 
 function displayNotes(data) {
     for (let i = 0; i < data.length; i++) {
-        let newNote = document.createElement('div');
-        let newTitle = document.createElement('h2');
-        let newBody = document.createElement('p');
-        newTitle.innerHTML = data[i].title;
-        newBody.innerHTML = data[i].body;
-        newNote.appendChild(newTitle);
-        newNote.appendChild(newBody);
-        newNote.classList.add('note');
-        notesAnchor.appendChild(newNote);
+        let noteEl = document.createElement('div');
+        let titleEl = document.createElement('h2');
+        let bodyEl = document.createElement('p');
+        let deleteBtn = document.createElement('button');
+        deleteBtn.id = 'deleteBtn';
+        deleteBtn.innerText = 'Delete';
+        titleEl.innerHTML = data[i].title;
+        bodyEl.innerHTML = data[i].body;
+        noteEl.appendChild(titleEl);
+        noteEl.appendChild(bodyEl);
+        noteEl.appendChild(deleteBtn);
+        noteEl.classList.add('note');
+        notesAnchor.appendChild(noteEl);
     }
 }
 
 
 saveBtn.addEventListener('click', function () {
-    let newNoteTitle = document.querySelector('#newNoteTitle').innerHTML;
-    let newNoteBody = document.querySelector('#newNoteBody').innerHTML;
+
+    // event.preventDefault()
+
+    let newNoteTitle = document.querySelector('#newNoteTitle').value;
+    let newNoteBody = document.querySelector('#newNoteBody').value;
 
     fetch('http://localhost:3000/notes/', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "title": newNoteTitle, "body": newNoteBody })
     })
-
 })
+
+
+
+function deleteNote(num) {
+    fetch('http://localhost:3000/notes/' + num, {
+        method: 'DELETE'
+    })
+}
