03fd0280d4da4516e3472f90837961b9c49a67ec
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-08-30 18:32:08 -0400

    added time to notes, starting to make an edit button

--- scripts.js
+++ scripts.js
@@ -14,15 +14,25 @@ window.addEventListener('load', function () {
 
 function displayNotes(data) {
     for (let i = 0; i < data.length; i++) {
-        let noteTime = document.createElement('p');
-        noteTime = new moment();
+
         let noteEl = document.createElement('div');
         let titleEl = document.createElement('h2');
         let bodyEl = document.createElement('p');
+        let timeEl = document.createElement('h3');
+        timeEl.innerHTML = data[i].time;
         titleEl.innerHTML = data[i].title;
         bodyEl.innerHTML = data[i].body;
         noteEl.appendChild(titleEl);
+        noteEl.appendChild(timeEl);
         noteEl.appendChild(bodyEl);
+
+        //make a new div at the bottom of noteEl class='buttons' that lets the edit and delete (and save, when editing) buttons be flex-wrap: wrap, and spaced nicely.
+
+        let editBtn = document.createElement('button');
+        editBtn.classList.add('editBtn');
+        editBtn.id = 'editBtn' + data[i].id;
+        editBtn.innerText = 'Edit';
+        noteEl.appendChild(editBtn);
 
         let deleteBtn = document.createElement('button');
         deleteBtn.classList.add('deleteBtn');
@@ -48,19 +58,24 @@ saveBtn.addEventListener('click', function () {
 
     let newNoteTitle = document.querySelector('#newNoteTitle').value;
     let newNoteBody = document.querySelector('#newNoteBody').value;
+    let noteTime = moment().calendar();
 
     fetch('http://localhost:3000/notes/', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ "title": newNoteTitle, "body": newNoteBody })
+        body: JSON.stringify({ "title": newNoteTitle, "body": newNoteBody, 'time': noteTime })
     })
 
     let noteEl = document.createElement('div');
     let titleEl = document.createElement('h2');
     let bodyEl = document.createElement('p');
+    let timeEl = document.createElement('h3');
+    timeEl.innerHTML = noteTime;
+    timeEl.id = 'timeEl';
     titleEl.innerHTML = document.querySelector('#newNoteTitle').value;
     bodyEl.innerHTML = document.querySelector('#newNoteBody').value;
     noteEl.appendChild(titleEl);
+    noteEl.appendChild(timeEl);
     noteEl.appendChild(bodyEl);
 
     noteEl.classList.add('note');
